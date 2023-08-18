8efd8b7abfd192ffca6397cb80e05dd68cf4d8f5
Levi Bevers <beverlev001@warren.k12.in.us>
2023-08-18 07:57:09 -0400

    U2Lesson4

---
+++ Unit2/Lesson04/assingment.py
@@ -0,0 +1,8 @@
+apples = 20
+oranges = 15
+print("Number  of apples:",apples)
+print("Number of oranges:",oranges)
+
+oranges = 0
+print("Number  of apples:",apples)
+print("Number of oranges:",oranges)
