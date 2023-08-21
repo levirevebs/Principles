3bc91ecee2f0a21150968e3e300779f406dc08be
Levi Bevers <beverlev001@warren.k12.in.us>
2023-08-21 08:07:39 -0400

    Lesson05 assingments

---
+++ Unit2/Lesson05/assn1.py
@@ -0,0 +1,4 @@
+cost = 12300
+bought = 93
+totalcost = cost * bought
+print("The total cost was $", totalcost, sep="")
---
+++ Unit2/Lesson05/assn2.py
@@ -0,0 +1,9 @@
+miles = 73
+minutes = 20
+hours = minutes / 60
+
+speed = miles / hours
+
+print("how many miles did you run?", miles)
+print("how man minutes did it take you?", minutes)
+print("speed in mph:",speed,"MPH")
