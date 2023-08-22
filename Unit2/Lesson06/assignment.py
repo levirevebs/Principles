d3e4b53081b08e53126e91b6491893972258cd46
Levi Bevers <beverlev001@warren.k12.in.us>
2023-08-22 07:53:14 -0400

    Lesson06 assignment

--- Unit2/Lesson06/assignment.py
+++ Unit2/Lesson06/assignment.py
@@ -1,4 +1,4 @@
 name = input("What is your name:")
 droids = input("How many droids do you want to meet:")
 wookiees = input("How many wookiees do you wan to meet:")
-print(name,"wants to meet",droids,"droids and",wookiees,"wookiees")
+print(name,"wants to meet",droids,"droids and",wookiees,"wookiees!")
