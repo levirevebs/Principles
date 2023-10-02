52c16a6b06e1a43feba1a318311984e01111dd43
Levi Bevers <beverlev001@warren.k12.in.us>
2023-10-02 07:54:14 -0400

    for loop

---
+++ Unit07/Lesson01/main.py
@@ -0,0 +1,15 @@
+from random import randint
+
+secret_number = randint(0, 20)
+
+user_guess = input("Enter a guess between 0 and 20")
+user_guess_num = int(user_guess)
+number_of_guesses = 1
+while user_guess_num != secret_number:
+    number_of_guesses += 1
+    #print some message that says you guessed wrong.  
+    #ask the user again to enter a guess and store in user_guess
+    #turn user_guess into user_guess_number
+
+print("Good job. The answer was", secret_number)
+print("Good job. It took you", number_of_guesses, "tries")
---
+++ Unit07/Lesson02/Main.py
@@ -0,0 +1,5 @@
+sum = 0
+for number in range(0, 101, 1):
+    print(number)
+
+print(sum)
