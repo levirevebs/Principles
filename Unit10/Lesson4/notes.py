8d5e5da12808865c361beb2d8682d400598a348a
Levi Bevers <beverlev001@warren.k12.in.us>
2023-11-16 07:58:38 -0500

    Lesson4 unit10

---
+++ Unit10/Lesson4/notes.py
@@ -0,0 +1,8 @@
+movies = ['Hunger Games', 'Home Alone', 'Polar Express', 'Godzilla', 'King Kong']
+movies.append('Holes')
+movies.append('Paranormal Activity')
+movies.append('The Upside')
+movies.append('Starwars')
+movies.append('back to the future')
+movies.insert(2,'Spongebob Movie')
+print(movies)
