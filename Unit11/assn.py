567dfa5ae0026c64935d1d233d513038cc537e05
Levi Bevers <beverlev001@warren.k12.in.us>
2023-11-30 08:03:41 -0500

    U11 functions

---
+++ Unit11/assn.py
@@ -0,0 +1,5 @@
+def function():
+    name = input("What is your name: ")
+    print(name.lower())
+function()
+function()
---
+++ Unit11/notes.py
@@ -0,0 +1,5 @@
+def function():
+    print('You have called the function.')
+
+function()
+
